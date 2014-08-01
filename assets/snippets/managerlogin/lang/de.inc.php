78201bc564de68f5dd67cd8745d2fd851d6e9d8e
Thomas Jakobi <thomas.jakobi@partout.info>
2014-08-01 18:31:05 +0200

    Added German Language

---
+++ assets/snippets/managerlogin/lang/de.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * German language file for ManagerLogin snippet
+ * Date: 02.01.2009 
+ */ 
+$_lang['username'] = 'Benutzername';
+$_lang['password'] = 'Passwort';
+$_lang['remember_username'] = 'Benutzername merken';
+$_lang['login_button'] = 'Anmelden';
+
+$_lang['err_user_notfound'] = 'Benutzername nicht gefunden.';
+$_lang['err_user_failedlogins'] = 'Ihr Account wurde gesperrt. Versuchen Sie es später noch einmal.';
+$_lang['err_user_blocked'] = 'Ihr Account wurde gesperrt.';
+$_lang['err_user_blockeduntil'] = 'Ihr Account wurde gesperrt. Versuchen Sie es später noch einmal.';
+$_lang['err_user_blockedafter'] = 'Ihr Account wurde gesperrt. Versuchen Sie es später noch einmal.';
+$_lang['err_user_ipnotallowed'] = 'Sie dürfen sich mit Ihrer IP-Adresse nicht anmelden.';
+$_lang['err_user_daynotallowed'] = 'Sie dürfen sich heute nicht anmelden.';
+$_lang['err_invalid_passwd'] = 'Passwort nicht korrekt.';
+
+$_lang['logged_msg'] = 'Sie sind schon angemeldet.';
+$_lang['logout_text'] = 'Abmelden';
+$_lang['home_text'] = 'Zurück zur Startseite';
+?>
